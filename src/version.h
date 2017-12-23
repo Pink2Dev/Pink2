435124057faf722d0947f7ee8608ca083eb401e3
JRedinger <saidge@yahoo.com>
2017-12-23 11:24:09 -0800

    Peer Version Requirements

--- src/version.h
+++ src/version.h
@@ -44,7 +44,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 60002;
-static const int NOBLKS_VERSION_END = 60013;
+static const int NOBLKS_VERSION_END = 60014;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
