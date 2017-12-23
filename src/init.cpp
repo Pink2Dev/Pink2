dfcc266afc144fdbb8fb58335628052d7f9fd6ad
JRedinger <saidge@yahoo.com>
2017-12-23 12:47:45 -0800

    Add Startfrom

--- src/init.cpp
+++ src/init.cpp
@@ -321,6 +321,7 @@ std::string HelpMessage()
         "  -keypool=<n>           " + _("Set key pool size to <n> (default: 100)") + "\n" +
         "  -rescan                " + _("Rescan the block chain for missing wallet transactions") + "\n" +
         "  -salvagewallet         " + _("Attempt to recover private keys from a corrupt wallet.dat") + "\n" +
+        "  -startfrom=<n>         " + _("Resync blockchain from a specific block number") +
         "  -checkblocks=<n>       " + _("How many blocks to check at startup (default: 500, 0 = all)") + "\n" +
         "  -checklevel=<n>        " + _("How thorough the block verification is (0-6, default: 1)") + "\n" +
         "  -loadblock=<file>      " + _("Imports blocks from external blk000?.dat file") + "\n" +
