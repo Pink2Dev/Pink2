db6c14ad5f46e0b1fcc75093c01f94c507625b44
opsinphark <53201159+opsinphark@users.noreply.github.com>
2021-03-07 05:07:25 +0100

    build: Increment VERSION, CLIENT_VERSION_SUFFIX, PROTOCOL_VERSION - Increments VERSION: 2.3.1.88 - Increments CLIENT_VERSION_SUFFIX: Rileys Revenge - Increments PROTOCOL_VERSION: 60019 - Increments MIN_PEER_PROTO_VERSION: 60018 (Riley)   * Drops connections with protocol versions less than 60018 (v2.3.1 Riley).     Riley was the mandatory hard fork that dropped Proof of Work and switched     to pure Proof of Stake as well as coin subsidy changes.  Connecting with     anything less is not necessary.

--- src/version.h
+++ src/version.h
@@ -29,14 +29,14 @@ static const int DATABASE_VERSION = 70509;
 //
 // network protocol versioning
 //
-
-static const int PROTOCOL_VERSION = 60018;
+//static const int PROTOCOL_VERSION = 60018;    // 2.3.1 (Riley)
+static const int PROTOCOL_VERSION = 60019;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 60017;
+static const int MIN_PEER_PROTO_VERSION = 60018;
 
 // Allow connections from old peers, intended for temporary use when a mandatory update is pending.
 static const int MIN_PEER_PROTO_VERSION_OLD = 60016;
