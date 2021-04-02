b3d7aba43785e5824669a0f2a2e93a39f93c1b69
opsinphark <53201159+opsinphark@users.noreply.github.com>
2021-04-02 10:31:22 +0200

    build: Fix project codename for the perfectionist/connoisseur (@thr)

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Pinkcoin");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   " Rileys Revenge"
+#define CLIENT_VERSION_SUFFIX   " Riley's Revenge"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
